656262d5af7449ddf43f2b6e55ec7d6201a25254
christianmarescalco <marescalcoc@gmail.com>
2022-07-28 21:32:27 +0200

    Sto provando clear

--- prova.py
+++ prova.py
@@ -1,14 +1,31 @@
-import asyncio
-import telegram
 
-
+from setuptools import Command
+from telegram.ext import Updater,CallbackContext, CommandHandler, MessageHandler, Filters
+from telegram import Update
+import logging
 TOKEN = "5247296052:AAEaZtE7W6mpDTrL754r3DCUAJZ59TYYn04"
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
 
 
-async def main():
-    bot = telegram.Bot("5247296052:AAEaZtE7W6mpDTrL754r3DCUAJZ59TYYn04")
-    async with bot:
-        print(await bot.get_me())
+updater = Updater(token = TOKEN, use_context= True)
+dispatcher = updater.dispatcher
 
-if __name__ == '__main__':
-    asyncio.run(main())
+def start(update: Update, context: CallbackContext):
+    context.bot.send_message(chat_id = update.effective_chat.id, text = "Ciao peppozzo 2")
+
+def echo(update: Update, context: CallbackContext):
+    context.bot.send_message(chat_id = update.effective_chat.id, text = update.message.text)
+
+def clear(update: Update, context: CallbackContext):
+    print(updater.bot.messages
+    .get_updates()[1])
+    #context.bot.deleteMessage(chat_id = update.effective_chat.id, message_id = update.message.message_id)
+
+start_handler = CommandHandler('start',start)
+clear_handler = CommandHandler('clear',clear)
+echo_handler =  MessageHandler(Filters.text & (~Filters.command), echo)
+dispatcher.add_handler(start_handler)
+dispatcher.add_handler(echo_handler)
+dispatcher.add_handler(clear_handler)
+
+updater.start_polling()
